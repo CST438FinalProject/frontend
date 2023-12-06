15a020427cc4fb7c1507b5a007cbae6c166ce318
Hmishra2412003 <hmishra@csumb.edu>
2023-12-06 14:15:20 -0800

    working react app

---
+++ airline/src/App.js
@@ -0,0 +1,15 @@
+// import React from "react";
+// import AvailableFlights from './AvailableFlights';
+import LoginPage from './components/LoginPage';
+import MainPage from './components/MainPage';
+import NavBar from './NavBar';
+import UserFlights from './UserFlights';
+// import './styles/style.css';
+
+function App() {
+  return (
+      <div className="App">
+        <LoginPage/>
+      </div>
+  );
+} export default App
